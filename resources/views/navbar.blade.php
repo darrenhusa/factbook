ee31ca17fd4142bcad027958ead12d0f4c849c99
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-08-16 16:49:15 -0500

    Add ccsj png and svg images. Update navbar for factbook.

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,11 +1,12 @@
 <nav id="header" class="fixed w-full z-10 top-0 bg-white border-b border-gray-400">
    <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-4">
       <div class="pl-4 flex items-center">
-         <svg class="h-5 pr-3 fill-current text-purple-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+         {{-- <svg class="h-5 pr-3 fill-current text-purple-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M0 2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm14 12h4V2H2v12h4c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2zM5 9l2-2 2 2 4-4 2 2-6 6-4-4z"/>
-         </svg>
-         <a class="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl"  href="#">
-         Help Article
+         </svg> --}}
+         <img src="/images/Seal-Crimson-2.svg" height="50px" width="150px" alt="ccsj seal crimson 2">
+         <a class="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl ml-4"  href="#">
+         Factbook 2020-2021
          </a>
       </div>
       <div class="block lg:hidden pr-4">
